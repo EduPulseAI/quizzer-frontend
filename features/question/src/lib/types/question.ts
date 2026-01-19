a8b7893c4fbf0dfa4bbb56655fa6ed6ec3a8bd9f
Devin Catuns <devincatuns1@gmail.com>
2026-01-19 13:08:19 -0500

    added session feature

---
+++ features/question/src/lib/types/question.ts
@@ -0,0 +1,35 @@
+/**
+ * [question]
+ * next-feature@0.1.1-beta.5
+ * January 11th 2026, 5:30:08 am
+ */
+export interface Question {
+  id: string;
+  text: String;
+  difficulty: Difficulty;
+  choices: AnswerChoice[];
+  tags: string[];
+}
+
+/**
+ * [difficulty]
+ * next-feature@0.1.1-beta.5
+ * January 11th 2026, 5:30:31 am
+ */
+export type Difficulty =
+  | 'BEGINNER'
+  | 'EASY'
+  | 'INTERMEDIATE'
+  | 'HARD'
+  | 'ADVANCED';
+
+/**
+ * [answer-choice]
+ * next-feature@0.1.1-beta.5
+ * January 11th 2026, 5:33:58 am
+ */
+export interface AnswerChoice {
+  choiceId: string;
+  value: string
+  isCorrect: boolean
+}
