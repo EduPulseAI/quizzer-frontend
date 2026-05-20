dff4caa0705d93b17bb5387eb8095aba5f5943fe
Devin Catuns <deecat10@gmail.com>
2026-05-20 01:54:47 -0400

    chore: implement legacy navigation items

--- features/sidebar/src/lib/constants/navigation.ts
+++ features/sidebar/src/lib/constants/navigation.ts
@@ -1,26 +1,30 @@
 import type { NavigationItem } from '../types';
 
-const profileNodes: NavigationItem[] = [
-    { name: 'Portfolio ', href: '/portfolio', icon: "briefcase", nodes: [] }
+const jobNodes: NavigationItem[] = [
+    { name: "Insights", href: "/insights", icon: "lightbulb", nodes: [] },
+
 ]
 const resumeNodes: NavigationItem[] = [
     { name: "Upload a resume", href: "/resume", icon: "upload", nodes: [] },
     { name: "Build existing resume", href: "/resume/build", icon: "wrench", nodes: [] },
+    { name: 'Profile', href: '/profile', icon: "briefcase", nodes: [] },
+    { name: 'Projects ', href: '/portfolio', icon: "briefcase", nodes: [] }
 ]
 
+const prepNodes: NavigationItem[] = [
+    { name: "Skills Analysis", href: "/skills", icon: "target", nodes:[] },
+    { name: "Learning", href: "/learning", icon: "book-open", nodes:[] },
+
+]
 /**
  * [app-navigation]
  * next-feature@0.1.2-3
  * February 24th 2026, 7:08:19 pm
  */
 export const APP_NAVIGATION = [
-    // { name: 'Portfolio', href: '/portfolio', icon: Briefcase, nodes: porfolioNodes },
-    { name: 'Profile', href: '/profile', icon: "briefcase", nodes: profileNodes },
-    // { name: "Skills Analysis", href: "/skills", icon: Target, nodes:[] },
-    // { name: "Job Matching", href: "/jobs", icon: TrendingUp, nodes:[] },
+    { name: "Jobs", href: "/jobs", icon: "trending-up", nodes:jobNodes },
     { name: "Resume", href: "/resume", icon: "file-text", nodes: resumeNodes },
-    { name: 'Interview', href: '/prep', icon: "message-square", nodes: [] },
-    // { name: "Learning", href: "/learning", icon: "book-open", nodes:[] },
+    { name: 'Interview', href: '/prep', icon: "message-square", nodes: prepNodes },
     { name: 'Settings', href: '/settings', icon: "settings", nodes: [] },
 ] as const satisfies NavigationItem[];
 
